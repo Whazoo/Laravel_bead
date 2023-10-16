93dd0040bee85036ad8fef1a78df558054a345e1
Whazoo <adiii873@gmail.com>
2023-10-16 18:35:39 +0200

    admin/user role elmentése nem jó

--- dev_nyilvantartas/app/Actions/Fortify/CreateNewUser.php
+++ dev_nyilvantartas/app/Actions/Fortify/CreateNewUser.php
@@ -24,12 +24,14 @@ class CreateNewUser implements CreatesNewUsers
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => $this->passwordRules(),
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
+            'status' => ['required', 'string', 'in:user,admin'], // Status validáció
         ])->validate();
 
         return User::create([
             'name' => $input['name'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
+            'status' => $input['status'], // Status hozzáadása
         ]);
     }
 }
