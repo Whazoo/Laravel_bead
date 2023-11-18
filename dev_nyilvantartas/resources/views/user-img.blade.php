e9105f6e55d798cdbada58370abb059f2acf6fad
Whazoo <adiii873@gmail.com>
2023-11-18 16:47:32 +0100

    noaccess szöveg megvan

--- dev_nyilvantartas/resources/views/user-img.blade.php
+++ dev_nyilvantartas/resources/views/user-img.blade.php
@@ -1,10 +1,33 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>STOP RIGHT THERE YOU CRIMINAL SCUM</title>
+    <style>
+        body {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+            margin: 0;
+        }
 
-<x-app-layout>
+        .centered-image {
+            text-align: center;
+        }
+        .centered-image img {
+            width: 85%; /* Set the width to 50% to scale down the image */
+            height: auto; /* Maintain the aspect ratio */
+        }
+    </style>
+</head>
+<body>
+    <div class="centered-image">
+        <x-single-image :image="url('images/noaccess.png')" />
+    </div>
+</body>
+</html>
 
 
-    <x-single-image :image="url('images/noaccess.png')" />
 
-
-
-</x-app-layout>
